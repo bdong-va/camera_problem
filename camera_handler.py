0d5d7e1fe75a46918305d9d5b28b667b5903f6e6
Bo Dong <zillionk1973@gmail.com>
2018-06-19 13:33:55 -0600

    adding helper method to make endpoint url

--- camera_handler.py
+++ camera_handler.py
@@ -1,5 +1,40 @@
 def camera_stats(camera_ids):
+    """
+    get camera image data from API calls, analysize it and return stats in json.
+    camera_ids: list of string, camera_id.
+    Return: json include stats info, example:
+    {
+        camera_ids:{
+            "most_data_use": "camera_id_one"
+            "highest_image":"camera_id_two"
+        }
+        "largest_image_list":[
+            {
+            camera_id: 1,
+            image: {
+                file_size: 42048,
+                },
+            }
+        ]
+    }
+    """
+    if not isinstance(camera_ids, list):
+        return {}
     if not camera_ids:
         return {}
 
     return {}
+
+def camera_url_maker(camera_ids):
+    """
+    take camera_ids list, return list of API endpoint list for these cameras.
+    """
+    if not isinstance(camera_ids, list):
+        return []
+    
+    urls = []
+    for camera_id in camera_ids:
+        url = "domain.com/camera/{}/".format(camera_id)
+        urls.append(url)
+    return urls
+
--- camera_hanlder_test.py
+++ camera_hanlder_test.py
@@ -1,10 +1,31 @@
 import unittest
-from camera_handler import camera_stats
+from camera_handler import camera_stats, camera_url_maker
 
 class TestCameraHandler(unittest.TestCase):
-    def test_dumb_test(self):
-        pass
-
     def test_return_empty_json_if_camera_ids_are_empty(self):
         stats = camera_stats([])
         self.assertEqual({}, stats)
+
+    def test_return_empty_json_if_camera_ids_is_not_list(self):
+        stats = camera_stats("not_list")
+        self.assertEqual({}, stats)
+        
+    
+
+class TestCameraUrlMaker(unittest.TestCase):
+    def test_return_empty_list_if_camera_ids_are_empty(self):
+        urls = camera_url_maker([])
+        self.assertEqual([], urls)
+    
+    def test_return_empty_json_if_camera_ids_is_not_list(self):
+        urls = camera_url_maker("not_list")
+        self.assertEqual([], urls)
+
+    def test_return_url_list_with_correct_camera_ids(self):
+        urls = camera_url_maker(["id_one", "id_two","id_three"])
+        expected = [
+            "domain.com/camera/id_one/",
+            "domain.com/camera/id_two/",
+            "domain.com/camera/id_three/",
+        ]
+        self.assertEqual(expected, urls)
